01395b90a03e8e49b80dfeb95c727b33e6872a5a
mobin mathew <mobinmathew888@gmail.com>
2025-04-08 22:56:06 +0530

    Remove .env from repo

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -4,19 +4,18 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC76s5AveqYgWIFRfnVuwIhxeT9UR4DBnw',
-  authDomain: 'olx-clone-a84e6.firebaseapp.com',
-  projectId: 'olx-clone-a84e6',
-  storageBucket: 'olx-clone-a84e6.firebasestorage.app',
-  messagingSenderId: '652799048458',
-  appId: '1:652799048458:web:f9d57ad336c063649758f3',
-  measurementId: 'G-WQKQC7S8ED',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-const storage = getStorage(app)
+const storage = getStorage(app);
 
-export {app, auth, db, storage}
+export { app, auth, db, storage };
